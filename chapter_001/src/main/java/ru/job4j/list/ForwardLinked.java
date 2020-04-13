699fdfe0dd5bfc728f6096d8ad8ffe5f0184b67b
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-04-13 18:24:31 +0300

    Перевернуть связанный список[#270328]

---
+++ chapter_001/src/main/java/ru/job4j/list/ForwardLinked.java
@@ -0,0 +1,80 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void revert() {
+        Node<T> head = null;
+        Node<T> intermediate = this.head;
+        Node<T> tail;
+        while (intermediate != null) {
+            tail = intermediate.next;
+            intermediate.next = head;
+            head = intermediate;
+            intermediate = tail;
+        }
+        this.head = head;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
+        linked.add(5);
+        linked.revert();
+        for (Integer integer : linked) {
+            System.out.println(integer);
+        }
+    }
+}
