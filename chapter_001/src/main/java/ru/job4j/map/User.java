267ec778ff153b4c0437613a668723b639a6e450
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-04-20 12:54:35 +0300

    Создать модель User[#270344]

---
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ *
+ */
+public class User {
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
