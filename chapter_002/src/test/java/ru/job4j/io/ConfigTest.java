7dbf0457bc20b40d08cd98c8b679713588ee9aac
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-05-03 23:28:23 +0300

    Читаем файл конфигурации [#270372]

--- chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -20,28 +20,6 @@ public class ConfigTest {
     }
 
     @Test
-    public void whenPairWithCommentBeforeValue1() {
-        String path = "./src/main/resources/app.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(
-                config.value("hiber"),
-                is("")
-        );
-    }
-
-    @Test
-    public void whenPairWithCommentBeforeValue2() {
-        String path = "./src/main/resources/app.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(
-                config.value("hibernate.connection.url"),
-                is("")
-        );
-    }
-
-    @Test
     public void whenPairWithCommentInValue() {
         String path = "./src/main/resources/app.properties";
         Config config = new Config(path);
