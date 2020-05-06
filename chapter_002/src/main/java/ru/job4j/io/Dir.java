27816d548a6571aa20577a2e7cef17e2b9ff6ecf
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-05-07 00:42:04 +0300

    File [#270371]

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Dir {
+    public static void main(String[] args) throws IOException {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.printf("folder - %s, size - %d%n", subfile.getName(), subfile.length());
+        }
+    }
+}
