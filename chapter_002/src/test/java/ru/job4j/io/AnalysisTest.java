d3241e5c74f6bfea3358066f521d4044e66b0660
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-05-06 23:51:04 +0300

    Тестирование IO [#270366]

--- chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,27 +1,37 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalysisTest {
-    @Test
-    public void unavailable() {
-        Analysis analysis = new Analysis();
-        String source = "./src/main/resources/server.log";
-        String target = "./src/main/resources/target.log";
-        analysis.unavailable(source, target);
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
-            assertThat(read.readLine(), is("10:57:01;10:59:01"));
-            assertThat(read.readLine(), is("11:01:02;11:02:02"));
-        } catch (Exception e) {
-            e.printStackTrace();
+    @Test
+    public void unavailableWithTempFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+           out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        String expected = "10:57:01;10:59:0111:01:02;11:02:02";
+        assertThat(rsl.toString(), is(expected));
     }
 }
