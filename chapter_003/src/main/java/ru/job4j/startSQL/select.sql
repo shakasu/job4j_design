f4a6c72cb11d5ffd38ab03098fb2791ae60b4508
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-06-29 22:47:50 +0300

    Фильтры [#270308]

---
+++ chapter_003/src/main/java/ru/job4j/startSQL/select.sql
@@ -0,0 +1,46 @@
+--table creation
+create table type (
+    id serial primary key,
+    name varchar (2000)
+);
+create table product (
+    id serial primary key,
+    name varchar (2000),
+    type_id int references type (id),
+    expired_date date,
+    price int
+);
+--put data in types
+insert into type (name) values ('СЫР');
+insert into type (name) values ('МОЛОКО');
+insert into type (name) values ('МОРОЖЕННОЕ');
+--put data in products
+insert into product (name, type_id, expired_date, price)
+values ('пискаревское молоко', 2, '2020-07-08 04:05:06', 50);
+
+insert into product (name, type_id, expired_date, price)
+values ('сыр гауда', 1, '2020-07-08 04:05:06', 200);
+
+insert into product (name, type_id, expired_date, price)
+values ('пломбир мороженное', 3, '2020-09-08 04:05:06', 70);
+
+--1. Написать запрос получение всех продуктов с типом "СЫР"
+select * from product where type_id = 1;
+--2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
+select * from product where name like '%мороженное%';
+--3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
+select * from product where expired_date between '2020-06-30' and '2020-07-31';
+--4. Написать запрос, который выводит самый дорогой продукт.
+select * from product where price = (SELECT MAX(price) FROM product);
+--5. Написать запрос, который выводит количество всех продуктов определенного типа.
+select count(*) from product where type_id = 1;
+--6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+select * from product where type_id = 1 or type_id = 2;
+--7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
+select t.name from product as p
+inner join type as t on p.type_id = t.id
+group by t.name
+having count(t.id) < 10;
+--8. Вывести все продукты и их тип.
+select * from product as p
+inner join type as t on p.type_id = t.id;
